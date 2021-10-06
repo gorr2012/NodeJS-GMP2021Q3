c4ef4a1afbcfe37d025347835aa2d984e8920e34
gorr2012 <sergey_gornost@mail.ru>
2021-10-06 12:33:23 +0300

    feat: implement task-2 with streams

--- task-1/services/reverseString.ts
+++ task-1/services/reverseString.ts
@@ -1,2 +1,22 @@
+import csv from 'csvtojson';
+import path from 'path';
+
 export const reverseString = (str: string) =>
-  ([...str] as string[]).reduceRight((acc, cur) => acc + cur, '')
+  ([...str] as string[]).reduceRight((acc, cur) => acc + cur, '');
+
+export const csvToTxt = (inputPath: string) =>
+  csv({ delimiter: ';' })
+  .fromFile(inputPath)
+  .subscribe(txtLine => {
+    txtLine;
+    delete txtLine['count'];
+  })
+  .on('error', errorHandler);
+
+export const errorHandler = (err: Error) => console.log('Something wrong has happened', err);
+
+export const getPaths = ([ , , inputName, outputName]: string[]) =>
+  ({
+    inputPath: path.join('csv', `${inputName}`),
+    outputPath: path.join('csv', `${outputName}`)
+  })
