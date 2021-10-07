2fdcd04eff049e573b2d265dd0b52eab78aff8cd
gorr2012 <sergey_gornost@mail.ru>
2021-10-07 14:18:46 +0300

    refactor: minor changes

--- task-1/services/services.ts
+++ task-1/services/services.ts
@@ -1,10 +1,9 @@
 import csv from 'csvtojson';
 import path from 'path';
 
-export const reverseString = (str: string) =>
-  ([...str] as string[]).reduceRight((acc, cur) => acc + cur, '');
+const reverseString = ([...str]: string[]) => str.reduceRight((acc, cur) => acc + cur, '');
 
-export const csvToTxt = (inputPath: string) =>
+const csvToTxt = (inputPath: string) =>
   csv({
     delimiter: ';',
     headers: ['book', 'author', 'amount', 'price']
@@ -16,10 +15,12 @@ export const csvToTxt = (inputPath: string) =>
     })
     .on('error', errorHandler);
 
-export const errorHandler = (err: Error) => console.log('Something wrong has happened', err);
+const errorHandler = (err: Error) => console.log('Something wrong has happened', err);
 
-export const getPaths = ([, , inputName, outputName]: string[]) =>
-({
-  inputPath: path.join('csv', `${inputName}`),
-  outputPath: path.join('csv', `${outputName}`)
-})
+const getPaths = ([, , inputName, outputName]: string[]) =>
+  ({
+    inputPath: path.join('csv', `${inputName}`),
+    outputPath: path.join('csv', `${outputName}`)
+  })
+
+export { reverseString, csvToTxt, errorHandler, getPaths }
