361854658f4b35aebeb0523ba58fbf008a4965c5
gorr2012 <sergey_gornost@mail.ru>
2021-11-23 22:33:57 +0300

    fix notes

--- src/controllers/deleteUser.ts
+++ src/controllers/deleteUser.ts
@@ -3,8 +3,8 @@ import { findAndDelete } from '../services/usersServices';
 
 const deleteUser = async (req: Request, res: Response) => {
   const { userId } = req.params;
-  await findAndDelete(userId);
-  res.status(200).json('User deleted');
+  const isDelete = await findAndDelete(userId);
+  isDelete ? res.status(200).json('User deleted') : res.status(404).json('Can not find such id');
 };
 
 export default deleteUser;
--- src/routes/usersRoutes.ts
+++ src/routes/usersRoutes.ts
@@ -1,7 +1,7 @@
-import { Router, json } from 'express';
+import { Router } from 'express';
 import { userSchema } from '../validate/validate';
 import { createValidator } from 'express-joi-validation';
-import { createUser, getAllUsers, getUserById, updateUser, deleteUser, handleOtherPaths } from '../controllers';
+import { createUser, getAllUsers, getUserById, updateUser, deleteUser } from '../controllers';
 
 export const usersRoutes = Router();
 const validator = createValidator();
@@ -9,7 +9,6 @@ const validator = createValidator();
 usersRoutes
   .get('/', getAllUsers)
   .get('/:userId', getUserById)
-  .post('/', json(), validator.body(userSchema), createUser)
-  .put('/', json(), validator.body(userSchema), updateUser)
-  .delete('/:userId', deleteUser)
-  .get('*', handleOtherPaths);
+  .post('/', validator.body(userSchema), createUser)
+  .put('/', validator.body(userSchema), updateUser)
+  .delete('/:userId', deleteUser);
