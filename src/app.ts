29e0a5bb5e921bb423a9c77a70f6827a24af8d42
gorr2012 <sergey_gornost@mail.ru>
2021-12-08 21:54:16 +0300

    feat: implement task 5.1

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,33 @@
-import express, { json } from 'express';
+import express, { json, Response, Request } from 'express';
+import { createLogger, format, transports } from 'winston';
 import cors from 'cors';
-import morgan from 'morgan';
-import { HOSTNAME, PORT } from './config';
+import { PORT } from './config';
 import routes from './routes/routes';
+import { NextFunction } from 'express-serve-static-core';
 
 const app = express();
+export const logger = createLogger({
+  transports: [
+    new transports.Console(),
+    new transports.File({
+      filename: 'combined.log',
+      format: format.combine(
+        format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+        format.align(),
+        format.printf((info) => `${info.level}: ${[info.timestamp]}: ${info.message}`)
+      ),
+    }),
+  ],
+});
 
 app.use(cors());
-app.use(morgan('tiny'));
 app.use(json());
 app.use(routes);
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.info(`Response status ${res.statusCode}`);
+});
 
-app.listen(PORT, () => console.log(`Running at ${HOSTNAME}:${PORT}`));
+app.listen(PORT, () => {
+  console.log('Server started...');
+  logger.info(`Server started and running on http://localhost:${PORT}`);
+});
